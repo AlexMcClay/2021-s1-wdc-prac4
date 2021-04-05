7d0f68ea1f5e05ebf22b43f777e921f41bcb44d8
AlexMcClay <64782687+AlexMcClay@users.noreply.github.com>
2021-04-06 00:22:20 +0930

    3.2 v1

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,43 @@ router.get('/last.txt', function(req, res, next) {
 
 });
 
+
+var counter = 0;
+router.get('/color.html', function(req, res, next) {
+  var color;
+
+  if (counter == 0) {
+    color = "red";
+    counter = 1;
+  }
+  else if (counter == 1) {
+    color = "yellow";
+    counter = 2;
+  }
+  else if (counter == 2) {
+    color = "green";
+    counter = 3;
+  }
+  else if (counter == 3) {
+    color = "blue";
+    counter = 0;
+  }
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+  <title>Express</title>
+  <link rel="stylesheet" href="/stylesheets/style.css">
+</head>
+
+<body>
+  <h1 style="color:${color};">${color}</h1>
+
+</body>
+
+</html> `);
+
+});
+
 module.exports = router;

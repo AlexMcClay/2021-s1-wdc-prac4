bdc2b36196f998282cfbadc5c5d50a16762523e0
AlexMcClay <64782687+AlexMcClay@users.noreply.github.com>
2021-04-05 20:39:57 +0930

    Update task 3.1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+var Prev_Date = " ";
+var date = " ";
+router.get('/last.txt', function(req, res, next) {
+    Prev_Date = date;
+    date = new Date();
+    res.send(Prev_Date);
+
+});
+
 module.exports = router;
